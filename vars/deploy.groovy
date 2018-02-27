f63ecaab7b49f4f29518e95847d243986ddbb75d
Nicolas Hurion <Nicolas.Hurion@fednot.be>
2018-02-27 13:28:36 +0100

    deploy step

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,20 @@
 #!/usr/bin/groovy
-def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+def call(String projectName, String jarName, String deploymentEnv = 'dev', String deploymentServer = '46.226.109.170', String deploymentUser = 'deploy') {
+
+    stage ("Deployment of ${projectName} on ${deploymentEnv}"){
+        steps {
+            script {
+                filePath = "/opt/projects/${deploymentEnv}/${projectName}/"
+            }
+            //input 'Do you approve the deployment?'
+            echo 'deploying...'
+            unstash "target"
+            sshagent (credentials: ['deploy_ssh']) {
+                sh "ssh -o StrictHostKeyChecking=no ${deploymentUser}@${deploymentServer} 'echo hello'"
+                sh "ssh -f ${deploymentUser}@${deploymentServer} 'pkill -e -f ${deploymentEnv}/${projectName} || true' "
+                sh "scp target/*.jar ${deploymentUser}@${deploymentServer}:${filePath}"
+                sh "ssh -f ${deploymentUser}@${deploymentServer} 'cd ${filePath} && nohup java -jar ${filePath}${jarName} & '"
+            }
+        }
+    }
 }
