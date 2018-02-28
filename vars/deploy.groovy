fc19bdca139938a2c89e350c17b3f3cb86cb178e
Nicolas Hurion <Nicolas.Hurion@fednot.be>
2018-02-28 10:55:52 +0100

    handle deployment approval

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,20 @@
 #!/usr/bin/groovy
 
-def call(String projectName, String jarName, String deploymentEnv = 'dev', String deploymentServer = '46.226.109.170', String deploymentUser = 'deploy') {
+def call(String projectName, String jarName, String deploymentEnv = 'dev', boolean approvalRequired = true, String deploymentServer = '46.226.109.170', String deploymentUser = 'deploy') {
     def filePath = "/opt/projects/${deploymentEnv}/${projectName}/"
-    //input "Do you approve the deployment of ${projectName} on ${deploymentEnv}?"
-    echo "Deploying ${projectName} on ${deploymentEnv}"
-    unstash "target"
-    sshagent(credentials: ['deploy_ssh']) {
-        sh "ssh -o StrictHostKeyChecking=no ${deploymentUser}@${deploymentServer} 'echo hello'"
-        sh "ssh -f ${deploymentUser}@${deploymentServer} 'pkill -e -f ${deploymentEnv}/${projectName} || true' "
-        sh "scp target/*.jar ${deploymentUser}@${deploymentServer}:${filePath}"
-        sh "ssh -f ${deploymentUser}@${deploymentServer} 'cd ${filePath} && nohup java -jar ${filePath}${jarName} & '"
+    def approved = !approvalRequired
+    if (approvalRequired) {
+        approved = input "Do you approve the deployment of ${projectName} on ${deploymentEnv}?"
+        echo "appovedd : ${approved}"
+    }
+    if (approved) {
+        echo "Deploying ${projectName} on ${deploymentEnv}"
+        unstash "target"
+        sshagent(credentials: ['deploy_ssh']) {
+            sh "ssh -o StrictHostKeyChecking=no ${deploymentUser}@${deploymentServer} 'echo hello'"
+            sh "ssh -f ${deploymentUser}@${deploymentServer} 'pkill -e -f ${deploymentEnv}/${projectName} || true' "
+            sh "scp target/*.jar ${deploymentUser}@${deploymentServer}:${filePath}"
+            sh "ssh -f ${deploymentUser}@${deploymentServer} 'cd ${filePath} && nohup java -jar ${filePath}${jarName} & '"
+        }
     }
 }
