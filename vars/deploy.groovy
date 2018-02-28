ea73353df9078605bafd9a1dc5f05f1a4c2a435c
Nicolas Hurion <Nicolas.Hurion@fednot.be>
2018-02-28 09:43:21 +0100

    improve syntax

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,15 +1,14 @@
 #!/usr/bin/groovy
 
-//def call(String projectName, String jarName, String deploymentEnv = 'dev', String deploymentServer = '46.226.109.170', String deploymentUser = 'deploy') {
-def call(Map config){
-    def filePath = "/opt/projects/${config.deploymentEnv}/${config.projectName}/"
+def call(String projectName, String jarName, String deploymentEnv = 'dev', String deploymentServer = '46.226.109.170', String deploymentUser = 'deploy') {
+    def filePath = "/opt/projects/${deploymentEnv}/${projectName}/"
     //input "Do you approve the deployment of ${projectName} on ${deploymentEnv}?"
-    echo "Deploying ${config.projectName} on ${config.deploymentEnv}"
+    echo "Deploying ${projectName} on ${deploymentEnv}"
     unstash "target"
     sshagent(credentials: ['deploy_ssh']) {
-        sh "ssh -o StrictHostKeyChecking=no ${config.deploymentUser}@${config.deploymentServer} 'echo hello'"
-        sh "ssh -f ${config.deploymentUser}@${config.deploymentServer} 'pkill -e -f ${config.deploymentEnv}/${config.projectName} || true' "
-        sh "scp target/*.jar ${config.deploymentUser}@${config.deploymentServer}:${filePath}"
-        sh "ssh -f ${config.deploymentUser}@${config.deploymentServer} 'cd ${filePath} && nohup java -jar ${filePath}${config.jarName} & '"
+        sh "ssh -o StrictHostKeyChecking=no ${deploymentUser}@${deploymentServer} 'echo hello'"
+        sh "ssh -f ${deploymentUser}@${deploymentServer} 'pkill -e -f ${deploymentEnv}/${projectName} || true' "
+        sh "scp target/*.jar ${deploymentUser}@${deploymentServer}:${filePath}"
+        sh "ssh -f ${deploymentUser}@${deploymentServer} 'cd ${filePath} && nohup java -jar ${filePath}${jarName} & '"
     }
 }
